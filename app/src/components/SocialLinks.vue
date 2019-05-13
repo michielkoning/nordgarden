7fc472c33b8228252c6d69dc08e23ddabb89b37e
Michiel Koning <michiel.koning@valtech.com>
2019-05-13 21:12:50 +0200

    accessibility

--- app/src/components/SocialLinks.vue
+++ app/src/components/SocialLinks.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="social-links">
-    <app-icon icon="amazon" :title="`${'viewNordgardenOn'} amazon`" />
-    <app-icon icon="spotify" :title="`${'viewNordgardenOn'} spotify`" />
-    <app-icon icon="youtube" :title="`${'viewNordgardenOn'} youtube`" />
-    <app-icon icon="last.fm" :title="`${'viewNordgardenOn'} last.fm`" />
-    <app-icon icon="facebook" :title="`${'viewNordgardenOn'} facebook`" />
-    <app-icon icon="twitter" :title="`${'viewNordgardenOn'} twitter`" />
-  </div>
+  <nav aria-labelledby="social-links-title">
+    <h2 class="sr-only">{{ $t('socialMediaLinks') }}</h2>
+    <ul class="social-links">
+      <li>
+        <a href="#">
+          <app-icon icon="amazon" :title="`${'viewNordgardenOn'} amazon`" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <app-icon icon="spotify" :title="`${'viewNordgardenOn'} spotify`" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <app-icon icon="youtube" :title="`${'viewNordgardenOn'} youtube`" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <app-icon icon="last.fm" :title="`${'viewNordgardenOn'} last.fm`" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <app-icon icon="facebook" :title="`${'viewNordgardenOn'} facebook`" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <app-icon icon="twitter" :title="`${'viewNordgardenOn'} twitter`" />
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -18,3 +45,11 @@ export default {
   },
 };
 </script>
+
+<style lang="postcss" scoped>
+ul {
+  @mixin list-reset;
+
+  display: flex;
+}
+</style>
