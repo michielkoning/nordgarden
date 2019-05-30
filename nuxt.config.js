25e2855d2652046ae60ede9bde103898c235483e
Michiel Koning <mail@michielkoning.nl>
2019-05-30 15:07:51 +0200

    favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,28 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { name: 'msapplication-TileColor', content: '#d7d7d7' },
+      { name: 'theme-color', content: '#d7d7d7' },
+
       {
         href: 'https://fonts.googleapis.com/css?family=ABeeZee',
         rel: 'stylesheet'
