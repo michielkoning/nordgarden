47ddc587869046e93c748ffe06468038c9dce7bc
Michiel Koning <mail@michielkoning.nl>
2019-05-30 14:20:57 +0200

    new spinner

--- components/AppLoader.vue
+++ components/AppLoader.vue
@@ -1,101 +1,69 @@
 <template>
   <div class="loader">
-    <span class="sr-only">{{ $t('loading') }}</span>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <span class="sr-only" aria-live="polite" role="region">{{
+      $t('loading')
+    }}</span>
   </div>
 </template>
 
 <style scoped lang="postcss">
 .loader {
-  background: gray;
-  border-top-left-radius: 2em;
-  border-top-right-radius: 2em;
-  width: 4em;
-  height: 2em;
-  overflow: hidden;
+  margin: 0 auto;
   position: relative;
-  margin: 0 auto 1em;
-
-  @nest button & {
-    margin: 0 1em 0 0;
-    font-size: 0.5em;
-    display: inline-block;
-    background: white;
-    transform: translateY(0.2em);
-
-    &::before, &::after {
-      background: $color-cta;
-    }
+  width: 64px;
+  height: 64px;
+}
+.loader div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: #fff;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.loader div:nth-child(1) {
+  left: 6px;
+  animation: loader1 0.6s infinite;
+}
+.loader div:nth-child(2) {
+  left: 6px;
+  animation: loader2 0.6s infinite;
+}
+.loader div:nth-child(3) {
+  left: 26px;
+  animation: loader2 0.6s infinite;
+}
+.loader div:nth-child(4) {
+  left: 45px;
+  animation: loader3 0.6s infinite;
+}
+@keyframes loader1 {
+  0% {
+    transform: scale(0);
   }
-
-  &::before,
-  &::after {
-    content: '';
-    background-color: white;
-    position: absolute;
-  }
-
-  &::before {
-    animation: spinner-gauge 4s infinite ease;
-    left: 50%;
-    margin-left: -2px;
-    bottom: 0.5em;
-    width: 4px;
-    height: 1.25em;
-    transform-origin: 50% 100%;
-  }
-
-  &::after {
-    border-radius: 50%;
-    left: 50%;
-    margin-left: -0.375em;
-    bottom: 0;
-    width: 0.75em;
-    height: 0.75em;
+  100% {
+    transform: scale(1);
   }
 }
-
-@keyframes spinner-gauge {
-  0%,
+@keyframes loader3 {
+  0% {
+    transform: scale(1);
+  }
   100% {
-    transform: rotate(-50deg);
+    transform: scale(0);
   }
-
-  10%,
-  80%,
-  86% {
-    transform: rotate(20deg);
+}
+@keyframes loader2 {
+  0% {
+    transform: translate(0, 0);
   }
-
-  20%,
-  24% {
-    transform: rotate(60deg);
-  }
-
-  40%,
-  70% {
-    transform: rotate(-20deg);
-  }
-
-  54%,
-  62% {
-    transform: rotate(70deg);
-  }
-
-  56% {
-    transform: rotate(78deg);
-  }
-
-  58% {
-    transform: rotate(73deg);
-  }
-
-  60% {
-    transform: rotate(75deg);
-  }
-
-  83%,
-  89% {
-    transform: rotate(25deg);
+  100% {
+    transform: translate(19px, 0);
   }
 }
 </style>
