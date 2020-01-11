2b936e5dda57c60879ebadd89b93efd4f58297d9
Michiel Koning <mail@michielkoning.nl>
2020-01-11 22:20:34 +0100

    tweak promo

--- components/AppPage.vue
+++ components/AppPage.vue
@@ -27,7 +27,9 @@ export default {
 
 <style lang="postcss" module>
 .main {
+  --triangle-size: 50vw;
   display: flex;
+  position: relative;
   min-height: 100vh;
   padding: 5em var(--gutter) 0;
   flex-direction: column;
@@ -35,6 +37,38 @@ export default {
   @media (--navigation-position-left) {
     margin-left: var(--width-navigation-left);
     padding-top: var(--gutter);
+  }
+
+  @media (--viewport-md) {
+    --triangle-size: 25vw;
+  }
+
+  &::before,
+  &::after {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    content: '';
+    z-index: -1;
+  }
+
+  &::after {
+    border-right: var(--triangle-size) solid transparent;
+    border-top: var(--triangle-size) solid var(--color-secondary);
+    width: 1em;
+    height: 1em;
+  }
+
+  &::before {
+    width: var(--triangle-size);
+    height: var(--triangle-size);
+    background: repeating-linear-gradient(
+      -45deg,
+      transparent,
+      transparent 18px,
+      #c5c5c5 18px,
+      #c5c5c5 20px
+    );
   }
 }
 
