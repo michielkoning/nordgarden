308aaa3a40cbb0fe179efb6b187f6ac6d807619a
Michiel Koning <mail@michielkoning.nl>
2020-02-07 21:12:07 +0100

    add redirects

--- components/AppBadge.vue
+++ components/AppBadge.vue
@@ -17,14 +17,14 @@ export default {
 .badge {
   font-size: var(--font-size-sm);
   background: var(--text-color);
-  min-width: 1.25em;
-  height: 1.25em;
+  min-width: 1.5em;
+  height: 1.5em;
+  border-radius: 1.5em;
   transform: translate3d(var(--spacing-xxs), 0, 0);
   flex: 0 1 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 1.25em;
   color: var(--color-secondary);
   padding: 0 0var (--spacing-xl);
 }
