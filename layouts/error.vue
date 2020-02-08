47808edaeab19d2ccd9ede6c41d39dd0c53d140b
Michiel Koning <mail@michielkoning.nl>
2020-02-08 14:23:51 +0100

    errorpage

---
+++ layouts/error.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" tabindex="-1">
+    <notch-wrapper>
+      <app-header />
+      <p>Error</p>
+      <error-handler />
+    </notch-wrapper>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import ErrorHandler from '@/components/ErrorHandler.vue'
+import NotchWrapper from '@/components/NotchWrapper.vue'
+
+export default {
+  components: {
+    NotchWrapper,
+    AppHeader,
+    ErrorHandler
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    title() {
+      if (this.error.statusCode === 404) {
+        return 'Pagina niet gevonden'
+      }
+      return 'An error occurred'
+    }
+  },
+  head() {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
