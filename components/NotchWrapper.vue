56ec716970a320bda1b8267ef31858f123dc059d
Michiel Koning <michiel.koning@valtech.com>
2020-02-11 15:26:23 +0100

    revert modules

--- components/NotchWrapper.vue
+++ components/NotchWrapper.vue
@@ -1,10 +1,10 @@
 <template>
-  <div :class="Sstyle['notch-wrapper']">
+  <div class="notch-wrapper">
     <slot />
   </div>
 </template>
 
-<style lang="postcss" module>
+<style lang="postcss">
 .notch-wrapper {
   padding: env(safe-area-inset-top) env(safe-area-inset-right)
     env(safe-area-inset-bottom) env(safe-area-inset-left);
