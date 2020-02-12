b0fbc15b350743603c18e31c8e9b15fc7ddb79df
Michiel Koning <mail@michielkoning.nl>
2020-02-12 20:35:56 +0100

    videos to data

--- pages/Videos.vue
+++ pages/Videos.vue
@@ -47,6 +47,7 @@ import { mapState } from 'vuex'
 import EventBusUtil from '@/utils/eventBusUtil'
 import IconPlay from '@/assets/icons/play.svg'
 import AppPage from '@/components/AppPage.vue'
+import videos from '~/data/videos'
 
 export default {
   components: {
@@ -59,61 +60,8 @@ export default {
   data() {
     return {
       title: this.$t('videos'),
-      videos: [
-        {
-          videoId: 'XqWaMrzyXyQ',
-          title: 'Tango'
-        },
-        {
-          videoId: 'bH67wt1CHv8',
-          title: 'SIDE OF THE ROAD – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: 'JRwpPplPqmM',
-          title: 'SIDE OF THE ROAD – LIVE @ TV RAI 1'
-        },
-        {
-          videoId: '3K-f6RVtKwA',
-          title: 'YOU MUST BE THE CHANGE – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: 'phujbtP9J_Y',
-          title: 'A MERRY OLD CHRISTMAS – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: '7qO4Lu5rxew',
-          title: 'LA MIA RIVOLUZIONE – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: 'Rc-fLsWAe_A',
-          title: 'LA CANZONE DEI CANI – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: '83OIrw0AjgY',
-          title: 'NON È LA CALIFORNIA – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: 'RA2dLa5ZAVY',
-          title: 'L’ABBANDONO – LIVE @ ROMA – 06.12.13'
-        },
-        {
-          videoId: 'ioYwLp2PKBY',
-          title: 'THESE LOVESICK BLUES – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: 'PBMpzxa-SJA',
-          title: 'KEEP IT SHINING ON THE INSIDE – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: '_SnVGrgAHmg',
-          title: 'THE PATH OF LOVE – (OFFICIAL VIDEO)'
-        },
-        {
-          videoId: '986m8vY9DVo',
-          title: 'GOOD THINGS DIE – (OFFICIAL VIDEO)'
-        }
-      ],
-      videoId: 'bH67wt1CHv8',
+      videos,
+      videoId: videos[0].videoId,
       playerVars: {
         rel: 0
       },
