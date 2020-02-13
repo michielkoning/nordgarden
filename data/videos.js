c8c9286c321558afafa52ae7ecd6be51f3eeddb7
Michiel Koning <michiel.koning@valtech.com>
2020-02-13 08:55:53 +0100

    capitalize videotitle

--- data/videos.js
+++ data/videos.js
@@ -1,54 +1,54 @@
 export default [
   {
     videoId: 'XqWaMrzyXyQ',
-    title: 'TANGO – (OFFICIAL VIDEO)'
+    title: 'Tango – (Official Video)'
   },
   {
     videoId: 'bH67wt1CHv8',
-    title: 'SIDE OF THE ROAD – (OFFICIAL VIDEO)'
+    title: 'Side Of The Road – (Official Video)'
   },
   {
     videoId: 'JRwpPplPqmM',
-    title: 'SIDE OF THE ROAD – LIVE @ TV RAI 1'
+    title: 'Side Of The Road – Live @ Tv Rai 1'
   },
   {
     videoId: '3K-f6RVtKwA',
-    title: 'YOU MUST BE THE CHANGE – (OFFICIAL VIDEO)'
+    title: 'You Must Be The Change – (Official Video)'
   },
   {
     videoId: 'phujbtP9J_Y',
-    title: 'A MERRY OLD CHRISTMAS – (OFFICIAL VIDEO)'
+    title: 'A Merry Old Christmas – (Official Video)'
   },
   {
     videoId: '7qO4Lu5rxew',
-    title: 'LA MIA RIVOLUZIONE – (OFFICIAL VIDEO)'
+    title: 'La Mia Rivoluzione – (Official Video)'
   },
   {
     videoId: 'Rc-fLsWAe_A',
-    title: 'LA CANZONE DEI CANI – (OFFICIAL VIDEO)'
+    title: 'La Canzone Dei Cani – (Official Video)'
   },
   {
     videoId: '83OIrw0AjgY',
-    title: 'NON È LA CALIFORNIA – (OFFICIAL VIDEO)'
+    title: 'Non È La California – (Official Video)'
   },
   {
     videoId: 'RA2dLa5ZAVY',
-    title: 'L’ABBANDONO – LIVE @ ROMA – 06.12.13'
+    title: 'L’abbandono – Live @ Roma – 06.12.13'
   },
   {
     videoId: 'ioYwLp2PKBY',
-    title: 'THESE LOVESICK BLUES – (OFFICIAL VIDEO)'
+    title: 'These Lovesick Blues – (Official Video)'
   },
   {
     videoId: 'PBMpzxa-SJA',
-    title: 'KEEP IT SHINING ON THE INSIDE – (OFFICIAL VIDEO)'
+    title: 'Keep It Shining On The Inside – (Official Video)'
   },
   {
     videoId: '_SnVGrgAHmg',
-    title: 'THE PATH OF LOVE – (OFFICIAL VIDEO)'
+    title: 'The Path Of Love – (Official Video)'
   },
   {
     videoId: '986m8vY9DVo',
-    title: 'GOOD THINGS DIE – (OFFICIAL VIDEO)'
+    title: 'Good Things Die – (Official Video)'
   }
 ]
