8ca73f307cec33ac0c459d9167c7e9af19e2759a
Michiel Koning <mail@michielkoning.nl>
2020-03-28 16:27:02 +0100

    remove announcer

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/vue-youtube', '~/plugins/axios', '~/plugins/announcer'],
+  plugins: ['~/plugins/vue-youtube', '~/plugins/axios'],
 
   /*
    ** Nuxt.js modules
